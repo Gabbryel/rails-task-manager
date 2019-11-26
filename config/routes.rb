7d1b23d1cb778699f4aed0cf6cd350a726135de0
Gabriel Ursache <gabriel@markov.ro>
2019-11-26 22:16:28 +0100

    style...almost

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "tasks", to: "tasks#index", as: :index
-  get "tasks/new", to: "tasks#new", as: :new_task
-  post "tasks", to: "tasks#create"
-  patch "task/:id", to: "tasks#update"
-  get "tasks/:id", to: "tasks#show", as: :task
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit
-  patch "tasks/:id", to: "tasks#update"
-  delete "tasks/:id", to: "tasks#delete"
+  get 'tasks', to: 'tasks#index', as: :index
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  patch 'task/:id', to: 'tasks#update'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#delete'
 end
